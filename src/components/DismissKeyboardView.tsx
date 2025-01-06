45c584602cdd4baa3e49c6f75a0f58064a3d2ba2
shleecloud <shleecloud@gmail.com>
2025-01-06 22:24:19 +0900

    feature: ch01

---
+++ src/components/DismissKeyboardView.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview';
+
+const DismissKeyboardView = ({
+  children,
+  ...props
+}: {
+  children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+}) => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <KeyboardAwareScrollView {...props} style={props.style}>
+      {children}
+    </KeyboardAwareScrollView>
+  </TouchableWithoutFeedback>
+);
+
+export default DismissKeyboardView;
